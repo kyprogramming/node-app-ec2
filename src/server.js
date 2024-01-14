bfb74241cb0d16efe4eb9bee383f2c7fdec99fcb
Kumar Programming <kkyprogramming@gmail.com>
2024-01-14 18:50:31 +0000

    my first commit

---
+++ src/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app  = express();
+
+app.get('/' , (req,res)=>{
+    res.send(`A simple node js applicion running on EC2 VM.`);
+});
+app.get('/contact' , (req,res)=>{
+    res.send(`please contact Kumar Programming @kyprogramming`);
+});
+
+const PORT = process.env.PORT || 80;
+
+app.listen(PORT , ()=>{
+    console.log(`Server started at port ${PORT}`);
+})
