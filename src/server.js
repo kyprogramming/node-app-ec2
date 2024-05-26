b9b19bfca52a2897b074467c48738217080f2900
Kumar Programming <kkyprogramming@gmail.com>
2024-05-26 21:15:38 +0100

    updated

--- src/server.js
+++ src/server.js
@@ -1,18 +1,24 @@
 const express = require('express');
+const path = require('path');
 
 const app  = express();
+const PORT = process.env.PORT || 80;
 
-app.get('/' , (req,res)=>{
-    res.send(`A simple node js applicion running on EC2 VM.`);
-});
+// app.get('/' , (req,res)=>{
+//     res.send(`A simple node js applicion running on EC2 VM.`);
+// });
 app.get('/contact' , (req,res)=>{
     res.send(`please contact Kumar Programming @kyprogramming`);
 });
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 // app.post('/github-webhook' , (req,res)=>{
 //     res.send(`webhook successfully returned.`);
 // });
-const PORT = process.env.PORT || 80;
+
+
 
 app.listen(PORT , ()=>{
     console.log(`Server started at port ${PORT}`);
